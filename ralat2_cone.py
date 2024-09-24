02efc8f0ef90f4b4c334e008f58893fe3233546b
Samuel-Sia-Jia-Ming <163949778+Samuel-Sia-Jia-Ming@users.noreply.github.com>
2024-09-24 09:24:03 +0800

    Update ralat2_cone.py 2

--- ralat2_cone.py
+++ ralat2_cone.py
@@ -1,23 +1,20 @@
-# Atur cara mengira luas permukaan kon.
+# Atur cara mengira pembahagian dua nombor.
 # Betulkan ralat dalam kod Python ini.
 
-import math
-pi = 3.142
+def calculation(a, b):    
+    division = a / b
+    return round(division, 2)
 
-def dapat_jejari_tinggi():
-    a = float(input("Masukkan jejari: "))
-    b = float(input("Masukkan tinggi: "))
-    return (d, c)
+def get_input():
+    x = int(input("Masukkan nombor integer pertama:"))
+    y = int(input("Masukkan nombor integer kedua:"))
+    return x, y
 
-def kira_luas_permukaan_kon(r, h):
-    luas_permukaan_kon = (pi * h) * (r + maths.squareroot(r**3 + h**3))
-    return round(luass_permukaan_kon, 2)
-
-def main_cone():
-    (x, y) = dapat_jejari_tinngi()
-    luas_permukaan_kon = kira_luas_permukaann_kon(x, z)
-    print(f"luas permukaan kon = {luas_permukaan_kon:.2f}")
+def main_calculation():
+    (x, y) = get_input( )
+    divide = calculation(x, y)    
+    print(f"Division = {divide}")
 
 # JANGAN ubah kod di bawah baris ini!
 if __name__ == "__main__":
-    main_cone()
+    main_calculation()
